045831beb20e58591ba13d4fef505e54908f4465
Peter Sjögren <peter.sjogren@pskonsult.se>
2019-11-25 12:01:52 +0100

    Some draggable svg:s

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import * as d3 from "d3";
 import Knob from "./components/Knob";
 import logo from "./logo.svg";
 import "./App.css";
+
+function makeDraggable(comp) {
+  let translateX = 0;
+  let translateY = 0;
+  const handleDrag = d3
+    .drag()
+    .subject(function() {
+      const me = d3.select(this);
+      return { x: translateX, y: translateY };
+    })
+    .on("drag", function() {
+      const me = d3.select(this);
+      const transform = `translate(${d3.event.x}, ${d3.event.y})`;
+      translateX = d3.event.x;
+      translateY = d3.event.y;
+      me.attr("transform", transform);
+    });
+  const node = ReactDOM.findDOMNode(comp);
+  handleDrag(d3.select(node));
+}
+
+class Circle extends React.Component {
+  componentDidMount() {
+    makeDraggable(this);
+  }
+  render() {
+    return <circle {...this.props} />;
+  }
+}
+class Rect extends React.Component {
+  componentDidMount() {
+    makeDraggable(this);
+  }
+  render() {
+    return <rect {...this.props} />;
+  }
+}
 
 class InputNumber extends React.Component {
   render(props) {
@@ -36,13 +75,21 @@ class App extends React.Component {
   render(props) {
     return (
       <div className="App">
-        <svg className="svgArea">
+        <svg
+          className="svgArea"
+          style={{ border: "1px solid" }}
+          width={300}
+          height={300}
+        >
+          >
           <circle
             cx={this.state.cx}
             cy={100}
             r={this.state.radius}
             fill="red"
           />
+          <Circle cx={50} cy={50} r={30} />
+          <Rect x={100} y={100} width={50} height={50} />
         </svg>
 
         <Knob
