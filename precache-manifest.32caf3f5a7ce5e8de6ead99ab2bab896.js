efb22b64b352c1813bae9e1ae3d497214508682d
Peter Sjögren <peter.sjogren@pskonsult.se>
2019-11-27 16:28:20 +0100

    Updates

---
+++ precache-manifest.32caf3f5a7ce5e8de6ead99ab2bab896.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4446ee26cc1cb43cb2048f679c6fde43",
+    "url": "/ps-react-test/index.html"
+  },
+  {
+    "revision": "355f25b78ba9d2c94995",
+    "url": "/ps-react-test/static/css/main.55ac22e9.chunk.css"
+  },
+  {
+    "revision": "a7a5e17bfed2c6453a13",
+    "url": "/ps-react-test/static/js/2.0839fede.chunk.js"
+  },
+  {
+    "revision": "355f25b78ba9d2c94995",
+    "url": "/ps-react-test/static/js/main.d9894bb7.chunk.js"
+  },
+  {
+    "revision": "c652b269db3177c5da82",
+    "url": "/ps-react-test/static/js/runtime-main.2b3dec66.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/ps-react-test/static/media/logo.25bf045c.svg"
+  }
+]);
