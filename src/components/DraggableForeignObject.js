cde01df6f6c881dc82ff6486d80db66fc366c14d
Peter Sjögren <peter.sjogren@pskonsult.se>
2019-12-25 09:10:59 +0100

    Added handle prop to ForeignDraggableObject

--- src/components/DraggableForeignObject.js
+++ src/components/DraggableForeignObject.js
@@ -4,7 +4,12 @@ import PropTypes from "prop-types";
 
 function DraggableForeignObject(props) {
   return (
-    <Draggable scale={props.scale} onDrag={props.onDrag} position={props}>
+    <Draggable
+      scale={props.scale}
+      onDrag={props.onDrag}
+      position={props}
+      handle={props.handle}
+    >
       <foreignObject x={0} y={0} width={props.width} height={props.height}>
         <div xmlns="http://www.w3.org/1999/xhtml">{props.children}</div>
       </foreignObject>
@@ -19,7 +24,8 @@ DraggableForeignObject.propTypes = {
   width: PropTypes.number.isRequired,
   height: PropTypes.number.isRequired,
   onDrag: PropTypes.func.isRequired,
-  children: PropTypes.any.isRequired
+  children: PropTypes.any.isRequired,
+  handle: PropTypes.string
 };
 
 export default DraggableForeignObject;
