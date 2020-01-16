d91bb11e88a22281e104afc1477b31c678198043
Peter Sjögren <peter.sjogren@pskonsult.se>
2020-01-16 23:32:33 +0100

    Example of snapshot test of a component

--- src/components/NodeList.test.tsx
+++ src/components/NodeList.test.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { NodeList } from "./NodeList";
+import renderer from "react-test-renderer";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
@@ -14,4 +15,17 @@ it("renders without crashing", () => {
     div
   );
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it("renders the same as last time", () => {
+  const component = renderer.create(
+    <NodeList
+      templates={[{ title: "One" }, { title: "Two" }]}
+      onLoadDefault={() => {}}
+      onLoadOther={() => {}}
+      onCreateNode={(index, title) => {}}
+    />
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
